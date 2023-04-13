dba8ab83a67b0991447180de3ca3fa7e7c8aad70
Veronika Voitovych <veronika.voitovych.tr.2021@lpnu.ua>
2023-04-14 00:13:28 +0300

    готова лр2

--- lab/Main.java
+++ lab/Main.java
@@ -17,5 +17,38 @@ public class Main {
     }
 
     System.out.println(y);
+    int X = (int)Math.round(Math.random() * (59 - 0) + 0);
+    System.out.println(X);
+    
+    int n = 0;
+
+    if( X <= 15){
+        n = 1;
+    } else if (X >= 16 && X <=30){
+        n = 2;
+    } else if (X >= 31 && X <=45){
+        n = 3;
+    } else if(X >= 46&& X <=59){
+        n = 4;
     }
-}
+
+    switch(n){
+        case 1:
+            System.out.println("Перша чверть");
+            break;
+        case 2:
+            System.out.println("Друга чверть");
+            break;
+        case 3:
+            System.out.println("Третя чверть");
+            break;
+        case 4:
+            System.out.println("Четверта чверть");
+            break;
+        default:
+            System.out.println("Помилка");
+    }}
+
+    }
+
+
